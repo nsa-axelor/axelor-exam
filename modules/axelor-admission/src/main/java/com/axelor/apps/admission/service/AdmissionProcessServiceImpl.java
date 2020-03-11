0c8a43247b7245e4b47f8422dc54f7d4545f05e2
nsa.axelor <nsa.axelor@gmail.com>
2020-03-11 17:47:25 +0530

    completed with seatTracker(counter)

--- modules/axelor-admission/src/main/java/com/axelor/apps/admission/service/AdmissionProcessServiceImpl.java
+++ modules/axelor-admission/src/main/java/com/axelor/apps/admission/service/AdmissionProcessServiceImpl.java
@@ -3,7 +3,9 @@ package com.axelor.apps.admission.service;
 import java.time.LocalDate;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.axelor.apps.admission.db.AdmissionEntry;
 import com.axelor.apps.admission.db.AdmissionProcess;
@@ -11,7 +13,7 @@ import com.axelor.apps.admission.db.College;
 import com.axelor.apps.admission.db.CollegeEntry;
 import com.axelor.apps.admission.db.FacultyEntry;
 import com.axelor.apps.admission.db.repo.AdmissionEntryRepository;
-import com.axelor.apps.admission.db.repo.FacultyEntryRepository;
+import com.axelor.apps.admission.db.repo.CollegeRepository;
 import com.axelor.inject.Beans;
 import com.google.inject.persist.Transactional;
 
@@ -28,6 +30,8 @@ public class AdmissionProcessServiceImpl implements AdmissionProcessService {
 				.bind("statusComfirmed", AdmissionEntryRepository.STATUS_CONFIRMED)
 				.bind("toDate", admissionProcess.getToDate()).fetch();
 
+		Map<Long, Map<Long, Integer>> collegeMap = initSeatTracker(admissionEntries);
+
 		for (AdmissionEntry entry : admissionEntries) {
 			List<CollegeEntry> collegeEntries = getSortedCollegeEntryList(entry.getCollegeEntryList());
 
@@ -35,35 +39,33 @@ public class AdmissionProcessServiceImpl implements AdmissionProcessService {
 				College college = collegeEntry.getCollege();
 				for (FacultyEntry facultyEntry : college.getFacultyEntryList()) {
 					if (entry.getFaculty().getId().equals(facultyEntry.getFaculty().getId())
-							&& facultyEntry.getSeats() > 0) {
-						if (!isCollegeSelected) {
-							entry.setCollegeSelected(college);
-							isCollegeSelected = true;
-							int cnt = facultyEntry.getSeats();
-							facultyEntry.setSeats(--cnt);
-							this.saveFacultyEntry(facultyEntry);
-						}
+							&& collegeMap.get(college.getId()).get(facultyEntry.getFaculty().getId()) > 0
+							&& !isCollegeSelected) {
+						
+						entry.setCollegeSelected(college);
+						isCollegeSelected = true;
+						Map<Long, Integer> facultyMap = collegeMap.get(college.getId());
+						int cnt = facultyMap.get(facultyEntry.getFaculty().getId());
+						facultyMap.put(facultyEntry.getFaculty().getId(), --cnt);
+						collegeMap.put(college.getId(), facultyMap);
+
 						entry.setValidationDate(LocalDate.now());
 						entry.setStatusSelect(AdmissionEntryRepository.STATUS_ADMITTED);
+						
 					} else if (entry.getFaculty().getId().equals(facultyEntry.getFaculty().getId())
-							&& facultyEntry.getSeats() <= 0)
+							&& collegeMap.get(college.getId()).get(facultyEntry.getFaculty().getId()) <= 0)
 						entry.setStatusSelect(AdmissionEntryRepository.STATUS_CANCELLED);
 				}
 			}
 			this.saveAdmissionEntry(entry);
 			isCollegeSelected = false;
 		}
-		
+
 	}
 
 	@Transactional
 	public void saveAdmissionEntry(AdmissionEntry admissionEntry) {
 		Beans.get(AdmissionEntryRepository.class).save(admissionEntry);
-	}
-
-	@Transactional
-	public void saveFacultyEntry(FacultyEntry facultyEntry) {
-		Beans.get(FacultyEntryRepository.class).save(facultyEntry);
 	}
 
 	public List<CollegeEntry> getSortedCollegeEntryList(List<CollegeEntry> collegeEntries) {
@@ -74,5 +76,21 @@ public class AdmissionProcessServiceImpl implements AdmissionProcessService {
 			}
 		});
 		return collegeEntries;
+	}
+
+	public Map<Long, Map<Long, Integer>> initSeatTracker(List<AdmissionEntry> admissionEntries) {
+		Map<Long, Map<Long, Integer>> collegeMap = new HashMap<>();
+
+		List<College> colleges = Beans.get(CollegeRepository.class).all().fetch();
+
+		for (College college : colleges) {
+			Map<Long, Integer> facultyMap = new HashMap<>();
+			for (FacultyEntry facultyEntry : college.getFacultyEntryList()) {
+				facultyMap.put(facultyEntry.getFaculty().getId(), facultyEntry.getSeats());
+			}
+			collegeMap.put(college.getId(), facultyMap);
+		}
+
+		return collegeMap;
 	}
 }
